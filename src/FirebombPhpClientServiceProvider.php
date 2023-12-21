0a92b504927f8b9ef4359fb4b98bef67010c9923
Alan <mmckalan@gmail.com>
2023-12-20 23:22:26 -0300

    Added: Test api

---
+++ src/FirebombPhpClientServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Exoticpixels\Firebomb;
+
+use Exoticpixels\Firebomb\FirebombExceptionHandler;
+use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
+class FirebombPhpClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publishing configuration file. Adjust the path as per your package structure.
+        $this->publishes([
+            __DIR__ . '/../config/firebombphpclient.php' => config_path('firebombphpclient.php'),
+        ]);
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->app->singleton(
+            ExceptionHandler::class,
+            FirebombExceptionHandler::class
+        );
+
+        // Merging the package's default configuration. Adjust the path as needed.
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/firebombphpclient.php',
+            'firebombphpclient'
+        );
+
+        // Bind the FirebombPhpExceptionLogger into the service container
+        $this->app->bind('firebombphpexceptionlogger', function ($app) {
+            return new FirebombPhpExceptionLogger(new Client());
+        });
+    }
+}
---
+++ src/FirebombPhpExceptionLogger.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Exoticpixels\Firebomb;
+
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+
+class FirebombPhpExceptionLogger
+{
+    protected $client;
+    protected $apiKey;
+    protected $url;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->apiKey = config('firebombphpclient.api_key');
+        $this->url = config('firebombphpclient.api_url');
+    }
+
+    /**
+     * Log an exception.
+     *
+     * @param Exception $exception The exception to log.
+     * @return void
+     */
+    public function logException(Exception $exception)
+    {
+        Log::info("Logging exception", ['exception' => $exception->getMessage()]);
+
+        try {
+            $this->client->post($this->url, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->apiKey,
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => [
+                    'message' => $exception->getMessage(),
+                    'trace' => $exception->getTraceAsString(),
+                ],
+            ]);
+        } catch (GuzzleException $e) {
+            Log::error('Failed to log exception', ['exception' => $e->getMessage()]);
+            // Add more detailed logging if necessary
+        }
+    }
+}
